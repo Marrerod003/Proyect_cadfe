721737587772dd88abdf16c67be9467d8e9427e4
Dev-Go <sccp.jyoungsug@gmail.com>
2025-07-03 18:12:44 -0500

    Primer push para subir proyecto spring boot Cdfe

---
+++ src/main/java/com/example/SistemaContableCdf/SistemaContableCdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaContableCdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaContableCdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaContableCdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SistemaContableCdf/SistemaContableCdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaContableCdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaContableCdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
